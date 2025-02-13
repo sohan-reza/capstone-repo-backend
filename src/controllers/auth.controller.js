1a759b3b3bac9d8bd9ef11aec5f9d6d87b578374
Zahidul Islam Mollik <md.zahidulmollik@gmail.com>
2025-02-13 18:17:06 +0600

    added jwt verify and logout API

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,8 @@ import { StatusCodes } from "http-status-codes";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+const blacklist = new Set();
+
 const registerUser=async(req,res)=>{
   try {
     const {email,password,role}=req.body;
@@ -83,8 +85,56 @@ const loginUser=async(req,res)=>{
         
   }
 }
+const verify=async(req,res)=>{
+  try {
+    const token = req.header('Authorization')?.split(' ')[1];
+    if (!token) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ status:false,message: "Token is required" });
+    }
 
-export const userController={
+    if (blacklist.has(token)) {
+      return res.status(403).json({ status:false,message: 'Token has been invalidated' });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.SECRET_KEY);
+      res.json({ status: true, user: decoded });
+    } catch (error) {
+      res.status(StatusCodes.UNAUTHORIZED).json({ status: false, message: "Invalid or expired token" });
+    }
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          status:false,
+          message:"something went wrong"
+        })
+        console.log(error);
+        
+  }
+}
+const logout =async(req,res)=>{
+  try {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if (token) {
+        blacklist.add(token);
+    }
+
+    res.json({ status:true, message: 'Logged out successfully' });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          status:false,
+          message:"something went wrong"
+        })
+        console.log(error);
+        
+  }
+}
+
+export const authController={
   registerUser,
-  loginUser
+  loginUser,
+  verify,
+  logout
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {userController} from "../controllers/auth.controller.js";
+import {authController} from "../controllers/auth.controller.js";
 import authReqValidation from "../middlewares/authReqValidation.js"
 const router = Router();
 
-router.post("/register",authReqValidation, userController.registerUser);
-router.post("/login",authReqValidation,userController.loginUser );
+router.post("/register",authReqValidation, authController.registerUser);
+router.post("/login",authReqValidation,authController.loginUser );
+router.post("/jwt/verify",authController.verify );
+router.post("/logout",authController.logout );
 
 export default router;
